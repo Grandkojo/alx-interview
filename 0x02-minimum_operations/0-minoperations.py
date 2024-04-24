627ebcc0ffc61f17ee70721071e2bdaaf951ed22
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-04-24 01:15:50 +0000

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Python minimum operations"""
+
+
+def minOperatons(n: int) -> int:
+    """
+    Finds the minimum operations to reach `n#`
+
+    Args:
+    - n: an integer determning the number of occurences
+
+    Returns:
+    - the minimum  number of operations
+    """
+
