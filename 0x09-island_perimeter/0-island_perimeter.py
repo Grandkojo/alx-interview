de9cd0dc7445530f1ba990a43e1c1caee6451cda
Grandkojo <essienernest.kojoowus@gmail.com>
2024-06-17 02:14:32 +0000

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
-""" the island perimeter function """
+"""This is the Island Perimeter interview Question"""
 
 
 def island_perimeter(grid):
-    """ the island perimeter class
-        grid: the grid to be used
-        returns: the permeter of the island
+    """_summary_
+
+    Args:
+        grid (_type_): _description_
     """
-
-    perimeter = 0
-    rows = len(grid)
-    cols = len(grid[0])
-
-    for row in range(0, rows-1):
-        for col in range(0, cols-1):
-            if grid[row][col] == 1:
-                # check top
-                if row == 0 or grid[row - 1][col] == 0:
-                    perimeter += 1
-                # check botttom
-                if row == rows - 1 or grid[row + 1][col] == 0:
-                    perimeter += 1
-                # check left
-                if col == 0 or grid[row][col - 1] == 0:
-                    perimeter += 1
-                # check right
-                if col == cols - 1 or grid[row][col + 1] == 0:
-                    perimeter += 1
-    return perimeter
+    ans = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                ans += 4
+                if i != 0 and grid[i-1][j] == 1:
+                    ans -= 1
+                if i < len(grid) - 1 and grid[i+1][j] == 1:
+                    ans -= 1
+                if j != 0 and grid[i][j-1] == 1:
+                    ans -= 1
+                if j < len(grid[0]) - 1 and grid[i][j+1] == 1:
+                    ans -= 1
+    return ans
