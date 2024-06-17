ac598a4871e04e5d61435d469e3e62a9602885f1
Grandkojo <essienernest.kojoowus@gmail.com>
2024-06-17 00:42:34 +0000

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" the island perimeter function """
+
+
+def island_perimeter(grid):
+    """ the island perimeter class
+        grid: the grid to be used
+        returns: the permeter of the island
+    """
+
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for row in range(0, rows-1):
+        for col in range(0, cols-1):
+            if grid[row][col] == 1:
+                # check top
+                if row == 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                # check botttom
+                if row == rows - 1 or grid[row + 1][col] == 0:
+                    perimeter += 1
+                # check left
+                if col == 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                # check right
+                if col == cols - 1 or grid[row][col + 1] == 0:
+                    perimeter += 1
+    return perimeter
